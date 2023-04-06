2c36f2102a9e06f4680217eb51e8b3ab220b93bc
Chris Aubin <69503979+chris-aubin@users.noreply.github.com>
2023-04-06 19:56:24 -0400

    Change format of relevant_positions.py result
    - Now hashes are keyed by (owner + tickLower + tickUpper) for easier use with simulator

--- relevant_positions.py
+++ relevant_positions.py
@@ -31,7 +31,7 @@ from eth_utils import to_checksum_address
 def get_relevant_positions(decoded_events):
     """Gets the addresses that called mint/ burn in decoded events """
 
-    positions = set()
+    positions = {}
     for event in decoded_events:
         if event["method"] in ["MINT", "BURN"]:
             # The key in a pool's position-indexed state is the keccak256 hash 
@@ -41,17 +41,19 @@ def get_relevant_positions(decoded_events):
             # https://github.com/Uniswap/v3-core/blob/d8b1c635c275d2a9450bd6a78f3fa2484fef73eb/contracts/libraries/Position.sol#L36
             # So we need to use the corresponding method from eth_abi:
             # https://eth-abi.readthedocs.io/en/latest/encoding.html
+            address   = to_checksum_address(event["method_data"]["owner"])
+            tickLower = event["method_data"]["tickLower"]
+            tickUpper = event["method_data"]["tickUpper"]
+            key       = str(address) + str(tickLower) + str(tickUpper)
             position_key_encoded = encode_packed(["address", "int24", "int24"],
-                [to_checksum_address(event["method_data"]["owner"]), 
-                event["method_data"]["tickLower"], 
-                event["method_data"]["tickUpper"]])
+                [address, tickLower, tickUpper])
             position_keccak = keccak.new(digest_bits = 256)
             position_keccak.update(position_key_encoded)
             position_key_hex = position_keccak.hexdigest()
             position_key_hex = "0x" + position_key_hex
-            positions.add(position_key_hex)
+            positions[key] = position_key_hex
     
-    return list(positions)
+    return positions
 
 
 def parse_args():
@@ -89,7 +91,7 @@ def main():
         args = parse_args()
         decoded_events = json.load(open(args["path_to_decoded_events"], "r"))
         data = get_relevant_positions(decoded_events["data"])
-        print(json.dumps({"data": data}, indent = 4))
+        print(json.dumps(data, indent = 4))
 
     except Exception as ex:
         print(ex, file=sys.stderr)
